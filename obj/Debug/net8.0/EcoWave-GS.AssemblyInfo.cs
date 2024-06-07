68efa505e0011e57f40e732eba59d184787a1b84
Vinishireis <nishimuravinicius28@gmail.com>
2024-06-07 17:46:09 -0300

    Update Code EcoWave Project

--- obj/Debug/net8.0/EcoWave-GS.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcoWave-GS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoWave-GS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234ceb96a9a663e9ebaf42da7e9dad82551cbed3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4c552e602c780c2017fc49cc884701880f95df")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoWave-GS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoWave-GS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
