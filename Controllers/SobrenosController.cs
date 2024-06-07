3e6eb65be002b4c18b043160ee2fc53282b623b8
MunirAyoub <rm550893@fiap.com.br>
2024-06-07 17:42:07 -0300

    aboutus

--- Controllers/SobrenosController.cs
+++ Controllers/SobrenosController.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using EcoWave_GS.Models;
 
 namespace EcoWave_GS.Controllers;
 
 public class SobrenosController
 {
-    public class SobrenosController : Controller
+    public class AboutUsController : Controller
     {
         public IActionResult AboutUs()
         {
